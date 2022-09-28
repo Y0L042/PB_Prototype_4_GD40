18d52c6730405d6a6f18d89ec8bf31a10fbc991d
Y0L042 <66077762+Y0L042@users.noreply.github.com>
2022-09-28 08:42:43 +0200

    Gridmap testing

---
+++ GridMap/GridMap.gd
@@ -0,0 +1,29 @@
+extends Node2D
+
+
+var tile_size: int
+var half_tile_size: int
+
+var grid_size: Vector2
+var grid: Array
+
+func _ready() -> void:
+	for x in range (grid_size.x):
+		gridd
+	pass
+
+func write_to_grid():
+	# Write data to grid
+	pass
+
+func read_from_grid():
+	# Read data from grid
+	pass
+
+func update_entity_position():
+	# Updates an entity's position
+	pass
+
+func vector_to_grid():
+	# Takes a global position and rounds it to a valid grid position
+	pass
